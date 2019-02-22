d2a418890960039610d33981faac64f69d846767
m-squared96 <michael.moore34@mail.dcu.ie>
2019-02-22 12:25:20 +0000

    Further works, rewriting signal_generator

--- signal_generator.py
+++ signal_generator.py
@@ -5,60 +5,59 @@ import os
 import numpy as np
 import pandas as pd
 
-#TODO: Rebuild to generate file based on a constant sampling frequency
-# Assume sampling frequency of about 1MHz 
+def signal_output(time_array,complexity,noise_strength,method,sampling_frequency):
+    
+    if method in ('wave','mkid'):
+        signal = np.zeros(len(time_array))
 
-def signal_output(time_array,complexity,noise_strength):
-    '''
-    Generates a Numpy array to specified length and complexity.
-    Output array will consist of a combination of sine waves and
-    random noise to a specified strength.
-    '''
+        if noise_strength < 1.0:
+            signal += noise_strength*np.random.random(len(signal))
 
-    if noise_strength > 1.0:
-        
-        raise ValueError("Noise strength should be less than or equal to 1")
+        elif noise_strength >= 1.0:
+            raise ValueError('Noise strength should be less than 1.0')
 
-    else:
-
-        signal = noise_strength*np.random.random(len(time_array))
-        
-        if type(complexity) == int:
-            coefficients = np.random.random(complexity)
+        if method == 'wave':
+            
+            coefficients = []
+            coeff_count = 1
+            while coeff_count <= complexity:
+                coefficients.append(np.random.randint(1,9))
+                coeff_count += 1
             coefficients = normalise(coefficients)
 
-        else:
-            try:
-                coefficients = np.random.random(int(complexity))
-                coefficients = normalise(coefficients)
+            frequencies = np.random.randint(low=1,high=5,size=complexity)*sampling_frequency/10
+            filename = "wave_" + str(complexity)
 
-            except:
-                raise ValueError("Complexity should be an integer")
-
-        freqs = np.random.randint(1,100,size=complexity)
-
-        for c,f in zip(coefficients,freqs):
+            for f,c in zip(coefficients,frequencies):
+                signal += c*np.sin(f*2*np.pi*time_array)
+                filename += "_" + str(f)[:4]
+        
+        elif method == 'mkid':
             
-            signal += c*np.sin(f*2*np.pi*time_array)
+            # Assuming a lower frequency limit of 2GHz, mixed down to ~ 0.2MHz
+            min_freq = 2e5
+            res_count = 1
+            res_freq = min_freq
 
-        print("Frequency values:")
-        print(freqs.T)
+            while res_count <= complexity:
+                signal += np.sin(res_freq*2*np.pi*time_array)
+                res_count += 1
+                res_freq += 2e3
 
-        filename = ""
+            max_freq = res_freq
 
-        for f in freqs:
-            
-            filename += str(f) + "_"
-
-        filename = filename[:-1]
+            filename = "mkid_" + str(complexity) + "_" + str(min_freq) + "_" + str(max_freq)
 
         return signal,filename
+
+    else:
+        raise ValueError('"method" parameter should be set to either "wave" or "mkid"')
 
 def normalise(vector):
     '''
     Takes a vector and returns the normalised vector
     '''
-
+    vector = np.array(vector)
     magnitude_s = 0
     for v in vector:
         magnitude_s += v**2
@@ -66,14 +65,6 @@ def normalise(vector):
     magnitude = np.sqrt(magnitude_s)
 
     return vector*(1/magnitude)
-
-def normalise_test():
-
-    vector = np.array([1,2,-1,0],float)
-
-    n = normalise(vector)
-
-    print(n)
 
 def directory_check():
 
@@ -83,31 +74,25 @@ def directory_check():
     '''
 
     if not(os.path.isdir("Data")):
-
         os.system("mkdir Data")
 
 def main():
 
     Npoint = 4096 
     multiple = 1000.789
-
     fs = 1e6 # Sampling frequency
     time_length = (Npoint*multiple)/fs
-
     time = np.arange(0,time_length,1/fs)
     
-    signal,filename = signal_output(time,1,0.0)
+    method = 'mkid'
+    signal, filename = signal_output(time,50,0.0,method,fs)
 
+    print(signal.shape)
     print(filename)
-
-    filename = 'SimpleLg'
-
     output_frame = pd.DataFrame({"Time":time, "Signal":signal})
-
     print("Signal data will be output to 'Data/' directory")
 
     directory_check()
-
     output_frame.to_csv("Data/" + filename + ".csv",index=False)
 
 main()
